cc2419e7bb64332821c33e396ae77dae1b962f75
fopi <peter.fodor86@gmail.com>
2016-02-29 23:34:05 +0100

    Generating random objects on the screen

--- assets/js/Game.js
+++ assets/js/Game.js
@@ -20,12 +20,36 @@ function init() {
     circle.x = 100;
     circle.y = 100;
     stage.addChild(circle);
-    createjs.Ticker.setFPS(30);
+    createjs.Ticker.setFPS(60);
+
+    function generateRandomPosition() {
+        return {
+            x: Math.ceil(Math.random() * config.canvas.width),
+            y: Math.ceil(Math.random() * config.canvas.height)
+        };
+    }
+
+    setInterval(function () {
+        var TitleView = myContainer.get('titleView');
+        var circle = new createjs.Graphics();
+        circle.setStrokeStyle(1);
+        circle.beginStroke("#000000");
+        circle.beginFill("red");
+        circle.drawCircle(0,0,30);
+
+        var shape = new createjs.Shape(circle);
+        var position = generateRandomPosition();
+        shape.x = position.x;
+        shape.y = position.y;
+        TitleView.addChild(shape);
+        stage.addChild(TitleView);
+        stage.update();
+    }, 1000);
 
     preloadAssets();
 
     function addMainView () {
-        var text = new createjs.Text("Hello World", "20px Arial", "#ff7700");
+        var text = new createjs.Text("Start game", "20px Arial", "#ff7700");
         var textDimensions = text.getBounds();
         text.x = (config.canvas.width / 2) - (textDimensions.width / 2);
         text.y = 100;
